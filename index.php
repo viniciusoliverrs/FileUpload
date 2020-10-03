69dab23e150394b976e0b5e7959d087cb15fca48
viniciusoliverrs <30914699+viniciusoliverrs@users.noreply.github.com>
2020-10-03 17:35:15 -0300

    Script para upload de arquivo

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+if (isset($_FILES['fileUpload'])) {
+    try {
+        $extension = strtolower(substr($_FILES['fileUpload']['name'], -4)); //Pegando extensão do arquivo
+        $name = date("d.m.Y-H.i.s") . $extension; //Definindo um novo nome para o arquivo
+        $dir = 'uploads/';
+        move_uploaded_file($_FILES['fileUpload']['tmp_name'], $dir . $name); //Fazer upload do arquivo
+    } catch (Exception $e) {
+        echo $e;
+    }
+}
+
+?>
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Upload</title>
+    <link rel="stylesheet" type="text/css" href="./assets/css/site.css" />
+</head>
+
+<body>
+    <form class="form-login" method="post" enctype="multipart/form-data">
+        <input type="file" name="fileUpload" />
+        <button style="margin-top:20px;">Enviar</button>
+    </form>
+</body>
+
+</html>
